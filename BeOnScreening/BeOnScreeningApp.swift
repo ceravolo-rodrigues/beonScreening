8f99d069daf4b5f84f382bba784b7b9c2da22abb
Paulo A. Cerávolo <paulo.ceravolo-ext@viavarejo.com.br>
2022-06-13 14:54:55 -0300

    Initial Commit

---
+++ BeOnScreening/BeOnScreeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeOnScreeningApp.swift
+//  BeOnScreening
+//
+//  Created by Paulo Antonio Ceravolo on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BeOnScreeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeOnScreening/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeOnScreening
+//
+//  Created by Paulo Antonio Ceravolo on 13/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
